81aa224916bde71e92523c38165a72c5b146ea0c
VictorEspejo <vicespejo96@gmail.com>
2022-05-06 13:52:04 +0200

    add logs in case fail token verification

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -14,6 +14,7 @@ const validarJWT = async (req = request, res = response, next) => {
         req.userAuth = userAuth;
         next();
     } catch (error) {
+        console.log(error);
         res.status(401).json({ msg: "Token no es valido" });
     }
  }
